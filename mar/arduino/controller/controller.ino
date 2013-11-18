9b14548a248474a70e9a46fbd24325e996f4b823
Gabriel Zea <zea@randomlab.net>
2013-11-17 20:46:06 -0500

    integrado sensor pir, fade in - out

--- mar/arduino/controller/controller.ino
+++ mar/arduino/controller/controller.ino
@@ -1,37 +1,113 @@
-#include <VirtualWire.h>
+const int interval = 10; //controla el tiempo del fade
 
-int projectors[] = {2,3,4,5,6,7,8};
-int projectors_num = 7;
+//pines
+const int pir = 22;
+const int projectors[] = {2,3,4,5,6,7,8};
+const int projectors_num = 7;
 
-int receiver = 22;
-int someone = 0;
-int velocity = 0;
+//flags
+boolean fadingIn = false;
+boolean fadeInDone = false;
+boolean fadingOut = false;
+boolean fadeOutDone = false;
 
-void setup() {
-    //inicializa los pines de salida y los apaga
-    for(int i=0; i<projectors_num; i++){
-        pinMode(projectors[i], OUTPUT);
-        digitalWrite(projectors[i], LOW);
-    }
+//tiempo y valor de los leds
+long time = 0;
+int value = 0;
 
-    //setup de comunicacion inalambrica
-    vw_set_rx_pin(receiver);   
-    vw_setup(2000); // Bits per sec
-    vw_rx_start(); // Start the receiver PLL running  
+
+void setup() { 
+  Serial.begin(9600);
+  Serial.println("Inicializando MAR");
+  
+  //inicializa los pines de salida y los apaga
+  for(int i=0; i<projectors_num; i++){
+      pinMode(projectors[i], OUTPUT);
+      digitalWrite(projectors[i], LOW);
+  }
+  
+  pinMode(pir, INPUT);
+  millis();
 }
 
-void loop() {
-    //comunicacion inalambrica
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-    if (vw_get_message(buf, &buflen)){
-        someone = buf[0];
-        velocity = buf[1];
-    }
+//funcion para escribir el valor actual en los pines
+void writeValue(int val){
+  for(int i=0; i<projectors_num; i++){
+      analogWrite(projectors[i], val);
+  }
+}
 
-    if(someone == 1){
-        for(int i=0; i<projectors_num; i++){
-            analogWrite(projectors[i], velocity);
-        }  
+void loop() { 
+  int someone = digitalRead(3);
+  
+  if(someone == LOW){ // hay alguien
+    
+    if(!fadingIn && !fadeInDone){
+      time = millis(); //toma el tiempo
+      fadingIn = true; //inicia el fadein
+      value = 0;
+      Serial.println("Inicia fadeIn");
     }
+    
+    if(!fadeInDone && fadingIn){ //Hace el fade
+      if(millis() - time > interval){ //aumenta el valor
+        time = millis();
+        value++;    
+      }
+
+      if(value >= 255){ //termina el fade y conserva value en rango
+        Serial.println("fadeIn terminado");
+        fadingIn = false;
+        fadeInDone = true;
+        
+        value = 255;
+      } else if (value < 0){
+        value = 0;
+      }
+    }
+    
+    if(fadeInDone){ //mantiene los leds encendidos
+       value = 255;
+    }
+    
+    writeValue(value);
+    
+    //cambia el estado de las banderas
+    fadingOut = false;
+    fadeOutDone = false;
+    
+  } else if (someone == HIGH){
+    //No hay nadie
+     
+    //cambia el estado para comenzar de nuevo
+    fadeInDone = false;
+    fadingIn = false;
+    
+    if(!fadingOut && !fadeOutDone){
+      Serial.println("no hay nadie, iniciando fadeOut");
+      time = millis(); //toma el tiempo
+      fadingOut = true; //inicia fadeOut
+    }
+    
+    if(fadingOut && !fadeOutDone){
+      if(millis() - time > interval){
+        time = millis();
+        value--;    
+      }
+
+      if(value <= 0){
+        Serial.println("fade out terminado");
+        value = 0;
+        fadingOut = false;
+      } else if (value >= 255){
+        value = 255;
+      }
+    }
+    
+    if(fadeOutDone){ //mantiene los leds encendidos
+       value = 255;
+    }   
+    
+    writeValue(value);
+  }  
 }
